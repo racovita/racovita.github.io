dff5aa61c4ad3260083c13a8dd418b0f296b1ce9
Calin Cascaval <cascaval@acm.org>
2023-03-26 13:23:24 +1300

    moved c4-theme inside

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source "https://rubygems.org"
 
-# If you have any plugins, put them here!
 group :jekyll_plugins do
   gem "jekyll-feed", "~> 0.12"
   gem "jekyll-seo-tag"
@@ -8,5 +7,3 @@ group :jekyll_plugins do
   gem "jekyll-redirect-from"
   gem "github-pages"
 end
-
-gem "c4-theme-jekyll", path: "../c4-theme-jekyll"
--- publish.sh
+++ publish.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-THEME_PATH="../c4-theme-jekyll"
+# THEME_PATH="../c4-theme-jekyll"
 TEST_SITE="http://test-web.racovita-foundation.org:88"
 REAL_SITE="https://www.racovita-foundation.org"
 
-pushd $THEME_PATH
-bundle exec jekyll build
-popd
+# pushd $THEME_PATH
+# bundle exec jekyll build
+# popd
 
 # replace the site URL with the test URL
 sed -i -e "s?url: \"$REAL_SITE\"?url: \"$TEST_SITE\"?" _config.yml
