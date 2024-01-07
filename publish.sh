24f0ed68d445cb7b2d648f1c983b43d0f621b0a1
Calin Cascaval <cascaval@acm.org>
2024-01-07 13:31:25 +1300

    platform independent publish script

--- publish.sh
+++ publish.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-# THEME_PATH="../c4-theme-jekyll"
 TEST_SITE="http://test-web.racovita-foundation.org:88"
 REAL_SITE="https://www.racovita-foundation.org"
 
-# pushd $THEME_PATH
-# bundle exec jekyll build
-# popd
+sedcmd="sed -i"
+if [[ $(uname -s) == "Darwin" ]]; then
+  sedcmd="${sedcmd} ''"
+fi
 
 # replace the site URL with the test URL
-sed -i -e "s?url: \"$REAL_SITE\"?url: \"$TEST_SITE\"?" _config.yml
+${sedcmd} -e "s?url: \"$REAL_SITE\"?url: \"$TEST_SITE\"?" _config.yml
 bundle exec jekyll build
-sed -i -e "s?url: \"$TEST_SITE\"?url: \"$REAL_SITE\"?" _config.yml
-rsync -avz _site/ saga.local:/var/www/racovita/
+${sedcmd} -e "s?url: \"$TEST_SITE\"?url: \"$REAL_SITE\"?" _config.yml
+rsync -avzu --delete --exclude="Cache" _site/ saga.local:/var/www/racovita/
